d10557563cb6c84ee3bea1dce4e714925a9efcfd
LucasLacerdaUX <lucas.lacerda@shopify.com>
2025-09-30 12:30:07 -0400

    Autocomplete address example.

--- extensions/address-autocomplete/shopify.d.ts
+++ extensions/address-autocomplete/shopify.d.ts
@@ -1,7 +1,7 @@
 import '@shopify/ui-extensions';
 
 //@ts-ignore
-declare module './src/Checkout.jsx' {
-  const shopify: import('@shopify/ui-extensions/purchase.checkout.block.render').Api;
+declare module './src/suggest.js' {
+  const shopify: import('@shopify/ui-extensions/purchase.address-autocomplete.suggest').Api;
   const globalThis: { shopify: typeof shopify };
 }
